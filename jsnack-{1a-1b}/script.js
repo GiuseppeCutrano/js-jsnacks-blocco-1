0b875599e1b313449c2905855c631b0e1b753eec
GiuseppeCutrano <giuseppecutrano.info@gmail.com>
2020-11-10 22:36:52 +0100

    first exs

---
+++ jsnack-{1a-1b}/script.js
@@ -0,0 +1,10 @@
+var palla ={
+  "nome": 'palla',
+  "peso": 10
+
+}
+console.log(palla)
+
+var inpUten = parseInt(prompt("Insersci il nuovo peso della palla"));
+palla.peso = inpUten
+console.log("il nuovo peso della palla è "+ palla.peso)
